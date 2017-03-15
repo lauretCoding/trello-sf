9c6684168629078792773ddcbd30f19048b07dfe
jeanlouis <lauret.jlouis@gmail.com>
2017-03-15 12:01:26 +0100

    Fix delete category

--- src/AppBundle/Manager/CategoryManager.php
+++ src/AppBundle/Manager/CategoryManager.php
@@ -43,6 +43,12 @@ class CategoryManager
         $this->entityManager->flush();
     }
 
+    public function remove(Category $category){
+        $this->entityManager->remove($category);
+        $this->entityManager->flush();
+    }
+
+
     /**
      * @return array
      */
